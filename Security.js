8ee62bb81a6fd27423921a4ccfcf4566f5f1b341
Maya2408 <84226035+Maya2408@users.noreply.github.com>
2021-10-13 19:46:06 +0530

    Add files via upload

---
+++ Security.js
@@ -0,0 +1,28 @@
+class Security {
+
+    constructor(){
+
+        this.access1 = createInput("Code1");
+        this.access1.position(100, 90);
+        this.access1.stly('background', 'white');
+
+        this.button1 = createButton("Check");
+        this.button1.position(100, 120);
+        this.button1.stly('background', 'lightgrey');
+
+
+    }
+
+    display(){
+
+        this.button1.mousePressed(() =>{
+            if(system.authenticate(accessCode1, this.access1.value())){
+                this.button1.hide();
+                this.access1.hide();
+                score++;
+            }
+        })
+
+
+    }
+}
